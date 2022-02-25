060fbd5f487319c0ff00430f5858efed2f6bf357
Lucas Briese <lucas.briese@iem.fraunhofer.de>
2022-02-25 15:26:40 +0100

    rm excluded code

--- lib/DB/ProjectIRDB.cpp
+++ lib/DB/ProjectIRDB.cpp
@@ -188,19 +188,7 @@ void ProjectIRDB::linkForWPA(llvm::Linker::Flags LinkerFlags) {
   // all modules.
   if (Modules.size() > 1) {
     llvm::Module *MainMod = getModuleDefiningFunction("main");
-    // <<<<<<< HEAD
-    //     // assert(MainMod && "could not find main function");
-    //     if (!MainMod) {
-    //       LOG_IF_ENABLE(BOOST_LOG_SEV(lg::get(), INFO)
-    //                     << "Main funciton not found and hence making an
-    //                     arbitrary "
-    //                        "[first on in the Modules map] "
-    //                        "module as the main module");
-    //       MainMod = Modules.begin()->second.get();
-    //     }
 
-    //     for (auto &[File, Module] : Modules) {
-    // =======
     if (!MainMod) {
       MainMod = Modules.begin()->second.get();
     }
