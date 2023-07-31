9891e9664463274a767d7bfd997f06d477be5dc4
mxHuber <huber.maximilian.leo@gmail.com>
2023-07-31 09:23:26 +0200

    basic structure

---
+++ lib/DataFlow/IfdsIde/Solver/IDESolverData.cpp
@@ -0,0 +1,4 @@
+#include "phasar/DataFlow/IfdsIde/Solver/IDESolverData.h"
+
+IDESolverData IDESolverData(const psr::LLVMProjectIRDB &IRDB,
+                            const nlohmann::json &Config) {}
