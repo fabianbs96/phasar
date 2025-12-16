5e36846e036b7c953e41f73c7bc9195bb9d3807a
Fabian Schiebel <fabianbs@mail.upb.de>
2025-12-16 08:15:43 +0100

    Small cleanup

--- tools/custom-container-test-tools/custom-container-test-tool.cpp
+++ tools/custom-container-test-tools/custom-container-test-tool.cpp
@@ -1,28 +1,20 @@
-/******************************************************************************
- * Copyright (c) 2017 Philipp Schubert.
- * All rights reserved. This program and the accompanying materials are made
- * available under the terms of LICENSE.txt.
- *
- * Contributors:
- *     Philipp Schubert and others
- *****************************************************************************/
-
+#include "phasar/DataFlow/IfdsIde/Solver/IFDSSolver.h"
+#include "phasar/PhasarLLVM/ControlFlow/LLVMBasedICFG.h"
+#include "phasar/PhasarLLVM/DB/LLVMProjectIRDB.h"
+#include "phasar/PhasarLLVM/DataFlow/IfdsIde/Problems/IFDSTaintAnalysis.h"
+#include "phasar/PhasarLLVM/HelperAnalyses.h"
+#include "phasar/PhasarLLVM/Pointer/LLVMAliasSet.h"
+#include "phasar/PhasarLLVM/SimpleAnalysisConstructor.h"
+#include "phasar/PhasarLLVM/TaintConfig/LLVMTaintConfig.h"
 #include "phasar/Utils/ChronoUtils.h"
-#include "phasar/Utils/SmallArraySet.h"
 #include "phasar/Utils/Timer.h"
-
-#include "phasar.h"
-
-#include <filesystem>
-#include <string>
 
 using namespace psr;
 
 int main(int Argc, const char **Argv) {
   using namespace std::string_literals;
 
-  if (Argc < 2 || !std::filesystem::exists(Argv[1]) ||
-      std::filesystem::is_directory(Argv[1])) {
+  if (Argc < 2) {
     llvm::errs() << "myphasartool\n"
                     "A small PhASAR-based example program\n\n"
                     "Usage: myphasartool <LLVM IR file>\n";
@@ -31,30 +23,30 @@ int main(int Argc, const char **Argv) {
 
   std::vector EntryPoints = {"main"s};
 
-  HelperAnalyses HA(Argv[1], EntryPoints);
+  HelperAnalyses HA(Argv[1], EntryPoints,
+                    {
+                        .CGTy = CallGraphAnalysisType::RTA,
+                    });
   if (!HA.getProjectIRDB().isValid()) {
     return 1;
   }
 
   if (const auto *F = HA.getProjectIRDB().getFunctionDefinition("main")) {
     // IFDS template parametrization test
-    llvm::outs() << "Testing IFDS:\n";
 
     auto DefaultConfig = LLVMTaintConfig(HA.getProjectIRDB());
 
     auto TaintProblem = createAnalysisProblem<IFDSTaintAnalysis>(
         HA, &DefaultConfig, EntryPoints);
-    // IFDSSolver S = IFDSSolver<LLVMIFDSAnalysisDomainDefault,
-    //                           SmallArraySet<const llvm::Value *>>(
-    //     TaintProblem, &HA.getICFG());
-    IFDSSolver S = IFDSSolver<LLVMIFDSAnalysisDomainDefault,
-                              SmallArraySet<const llvm::Value *>>(
-        TaintProblem, &HA.getICFG());
+
+    IFDSSolver S(TaintProblem, &HA.getICFG());
 
     Timer TimeSolve = Timer([](psr::hms ElapsedTime) {
       llvm::errs() << "IFDSResults ElapsedTime: " << ElapsedTime << "\n";
     });
-    auto IFDSResults = S.solve();
+
+    llvm::errs() << "Testing IFDS:\n";
+    S.solve();
   } else {
     llvm::errs() << "error: file does not contain a 'main' function!\n";
   }
