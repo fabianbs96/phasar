426345d345ad3d57f1d1f81901e1d54d8e9f217f
mxHuber <huber.maximilian.leo@gmail.com>
2026-09-10 15:57:50 +0200

    added xtaint lit test to both taint_analysis/double_free tests

--- test/llvm_test_code/taint_analysis/double_free_01.c
+++ test/llvm_test_code/taint_analysis/double_free_01.c
@@ -6,6 +6,9 @@ int main() {
   free(X);
 }
 
+// RUN: %S/../../../build/tools/phasar-cli/phasar-cli --data-flow-analysis=ide-xtaint --module %S/../../../build/test/llvm_test_code/taint_analysis/double_free_01_c_dbg.ll --analysis-config %S/../../../config/double-free-config.json | /usr/local/llvm-16/bin/FileCheck %s -check-prefix=xtaint
+// xtaint: /taint_analysis/double_free_01.c:6:3:
+
 // RUN: %S/../../../build/tools/phasar-cli/phasar-cli --data-flow-analysis=ifds-taint --module %S/../../../build/test/llvm_test_code/taint_analysis/double_free_01_c_dbg.ll --analysis-config %S/../../../config/double-free-config.json | /usr/local/llvm-16/bin/FileCheck %s -check-prefix=ifds-taint
 // ifds-taint: /taint_analysis/double_free_01.c:6:3:
 
--- test/llvm_test_code/taint_analysis/double_free_02.c
+++ test/llvm_test_code/taint_analysis/double_free_02.c
@@ -8,6 +8,9 @@ int main() {
   free(X);
 }
 
+// RUN: %S/../../../build/tools/phasar-cli/phasar-cli --data-flow-analysis=ide-xtaint --module %S/../../../build/test/llvm_test_code/taint_analysis/double_free_02_c_dbg.ll --analysis-config %S/../../../config/double-free-config.json | /usr/local/llvm-16/bin/FileCheck %s -check-prefix=xtaint
+// xtaint: A LLVM-based static analysis framework
+
 // RUN: %S/../../../build/tools/phasar-cli/phasar-cli --data-flow-analysis=ifds-taint --module %S/../../../build/test/llvm_test_code/taint_analysis/double_free_02_c_dbg.ll --analysis-config %S/../../../config/double-free-config.json | /usr/local/llvm-16/bin/FileCheck %s -check-prefix=ifds-taint
 // ifds-taint: /taint_analysis/double_free_02.c:8:3:
 
